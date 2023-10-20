a31e04b15d616377d2e62e26d39cfb21838b07c4
Andreas Wiesinger <andreas.wiesinger@rise-world.com>
2023-10-20 20:18:22 +0200

    First basic heighfieldmap compute shader update in log putout

--- shaders/water_compute.glsl
+++ shaders/water_compute.glsl
@@ -1,17 +1,50 @@
 #[compute]
 #version 450
 
-// Invocations in the (x, y, z) dimension
-layout(local_size_x = 2, local_size_y = 1, local_size_z = 1) in;
+layout(local_size_x = 16, local_size_y = 16, local_size_z = 1) in;
 
-// A binding to the buffer we create in our script
-layout(set = 0, binding = 0, std430) restrict buffer MyDataBuffer {
-    float data[];
+layout(set = 0, binding = 0, std430) restrict buffer HeightDataBuffer {
+    float heightmap[];
+} height_data_buffer;
+
+layout(set = 0, binding = 1, std430) restrict buffer VelocityDataBuffer {
+    vec2 velocity[];
+} velocity_data_buffer;
+
+float get_height(int x, int y, int width) {
+    return height_data_buffer.heightmap[y * width + x];
 }
-my_data_buffer;
 
-// The code we want to execute in each invocation
 void main() {
-    // gl_GlobalInvocationID.x uniquely identifies this invocation across all work groups
-    my_data_buffer.data[gl_GlobalInvocationID.x] *= 2.0;
+    uvec3 gid = gl_GlobalInvocationID;
+    // int width = int(gl_WorkGroupSize.x * gl_NumWorkGroups.x);
+    // int height = int(gl_WorkGroupSize.y * gl_NumWorkGroups.y);
+    int width = 3;
+    int height = 3;
+
+    if(gid.x >= width || gid.y >= height) return;
+
+    int x = int(gid.x);
+    int y = int(gid.y);
+    
+    // Neighbor heights
+    float hLeft = get_height(x - 1, y, width);
+    float hRight = get_height(x + 1, y, width);
+    float hUp = get_height(x, y - 1, width);
+    float hDown = get_height(x, y + 1, width);
+    
+    // Current height and velocity
+    float hCurrent = get_height(x, y, width);
+    vec2 velocity = velocity_data_buffer.velocity[y * width + x];
+
+    // Update logic here
+    // ...
+
+    // For example, update height based on some factor of neighboring heights and current velocity
+    float newHeight = hCurrent + 0.1 * (hLeft + hRight + hUp + hDown - 4.0 * hCurrent);
+
+    // Update height
+    height_data_buffer.heightmap[y * width + x] = newHeight;
+
+    // Similarly, you can update the velocity_data_buffer as per your simulation needs
 }
